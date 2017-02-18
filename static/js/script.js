66ec55b6ad71ab40739e7529d7451528f7cadbc7
Ben <bsadick@gmail.com>
2017-02-18 17:47:31 +0900

    jquery added, basic event listening added

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,24 @@
-window.alert('hello')
+let tasks = ['Go to store','Buy Bacon','Eat Bacon'];
+
+$(document).ready(() => {
+  populate();
+  addEventListeners();
+});
+
+function populate() {
+  let $tasks = $('#taskList');
+  $tasks.html('');
+  for (let i = 0; i < tasks.length; i++) {
+    let $task = $('<li>').text(tasks[i]);
+    $tasks.append($task);
+  }
+}
+
+function addEventListeners() {
+  $('#btnAdd').click(() => {
+    let $inAdd = $('#inAdd').val();
+    tasks.push($inAdd);
+    populate();
+    window.alert("added " + $inAdd);
+  });
+}
