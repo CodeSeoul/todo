689d6ec9a1344b3c115a279bd71b29440d8c1085
Ben <bsadick@gmail.com>
2017-02-18 16:34:02 +0900

    developed index.html

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
-  res.end('Hello, World');
+  fs.readFile('static/index.html', function(err, data){
+    if(err){
+      console.log("couldn't read file");
+    }else{
+      res.end(data);
+    }
+  })
 });
 
 server.listen(3000, () => console.log('running on 3000'));
