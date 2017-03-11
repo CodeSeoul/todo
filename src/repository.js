158ce7be243247624bb0401589686b248bec0d83
Dale Seo <dale.seo@gmail.com>
2017-03-11 15:45:29 +0900

    standard linter

--- src/repository.js
+++ src/repository.js
@@ -1,27 +1,25 @@
-const fs = require('fs');
+const fs = require('fs')
+const path = require('path')
 
 class Repository {
-
-  findTasks(callback) {
-    console.log('findTasks');
-    fs.readFile(__dirname + '/tasks.dat', (err, data) => {
-      if(err){
-        console.log(err);
-      }else{
-        callback(data);
+  findTasks (callback) {
+    console.log('findTasks')
+    fs.readFile(path.join(__dirname, 'tasks.dat'), (err, data) => {
+      if (err) {
+        console.log(err)
+      } else {
+        callback(data)
       }
-    });
+    })
   }
 
-  addTask() {
-    console.log('addTask');
-
+  addTask () {
+    console.log('addTask')
   }
 
-  deleteTask() {
-    console.log('deleteTask');
+  deleteTask () {
+    console.log('deleteTask')
   }
-
 }
 
-module.exports = Repository;
+module.exports = Repository
--- static/js/script.js
+++ static/js/script.js
@@ -1,48 +1,48 @@
-let tasks = ['Go to store','Buy Bacon','Eat Bacon','Look at bacon','Think about Bacon'];
+let tasks = ['Go to store', 'Buy Bacon', 'Eat Bacon', 'Look at bacon', 'Think about Bacon']
 
 $(document).ready(() => {
-  populate();
-  addEventListenersToAdd();
-  addEventListenersToRemove();
+  populate()
+  addEventListenersToAdd()
+  addEventListenersToRemove()
   // addEventLIstenersToTrash();
-  $('#btnAjax').click(callAjax);
-});
+  $('#btnAjax').click(callAjax)
+})
 
-function populate() {
-  let $tasks = $('#taskList');
-  $tasks.html('');
+function populate () {
+  let $tasks = $('#taskList')
+  $tasks.html('')
   for (let i = 0; i < tasks.length; i++) {
-    let $task = $('<li class="list-group-item">');
+    let $task = $('<li class="list-group-item">')
     let taskHtml = `<input type="checkbox" name="checkRemove" id="${i}" style="align:left">` +
-      tasks[i] + `<button name="trash-icon" id="${tasks[i]}" onclick="removeTask('` + tasks[i] + `')"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>`;
-    $task.html(taskHtml);
-    $tasks.append($task);
+      tasks[i] + `<button name="trash-icon" id="${tasks[i]}" onclick="removeTask('` + tasks[i] + `')"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>`
+    $task.html(taskHtml)
+    $tasks.append($task)
   }
 }
 
-function addEventListenersToAdd() {
+function addEventListenersToAdd () {
   $('#btnAdd').click(() => {
-    let $inAdd = $('#inAdd').val();
-    let btnValidity = checkValidityButtons($inAdd, "a");
+    let $inAdd = $('#inAdd').val()
+    let btnValidity = checkValidityButtons($inAdd, 'a')
     if (btnValidity === true) {
-      tasks.push($inAdd);
-      populate();
-      console.log("added " + $inAdd);
+      tasks.push($inAdd)
+      populate()
+      console.log('added ' + $inAdd)
     } else {
-      window.alert(btnValidity);
+      window.alert(btnValidity)
     }
-  });
+  })
 }
 
-function addEventListenersToRemove() {
-  $('#btnRemove').click(function() {
-    let checkedItems = $('input[type="checkbox"]:checkbox:checked');
+function addEventListenersToRemove () {
+  $('#btnRemove').click(function () {
+    let checkedItems = $('input[type="checkbox"]:checkbox:checked')
     for (let i = 0; i < checkedItems.length; i++) {
-      let id = checkedItems[i - i].id;
-      tasks.splice(id, 1);
+      let id = checkedItems[i - i].id
+      tasks.splice(id, 1)
     }
-    populate();
-  });
+    populate()
+  })
 }
 
 // function addEventLIstenersToTrash() {
@@ -52,30 +52,30 @@ function addEventListenersToRemove() {
 //   });
 // }
 
-function removeTask(id) {
-  let index = tasks.indexOf(id);
-  tasks.splice(index, 1);
-  populate();
+function removeTask (id) {
+  let index = tasks.indexOf(id)
+  tasks.splice(index, 1)
+  populate()
 }
 
-function checkValidityButtons($inAdd, op) { // @: $inAdd: texture input, op: operation code
-  if ($inAdd.length < 1) return "Opps, Todo shouldn't be blank";
-  if ($inAdd.length > 70) return "Todo should be less than 70 characters, yours is " + $inAdd.length + ".";
+function checkValidityButtons ($inAdd, op) { // @: $inAdd: texture input, op: operation code
+  if ($inAdd.length < 1) return "Opps, Todo shouldn't be blank"
+  if ($inAdd.length > 70) return 'Todo should be less than 70 characters, yours is ' + $inAdd.length + '.'
 
-  let isTaskInList = tasks.includes($inAdd);
+  let isTaskInList = tasks.includes($inAdd)
 
-  if (op == "a" && isTaskInList) { // Checks Add Button
-    return "Oops, Task is already in the list";
-  } else if (op == "r" && !isTaskInList) { //Checks Remove Buton
-    return "Oops, Task is NOT in the list";
+  if (op === 'a' && isTaskInList) { // Checks Add Button
+    return 'Oops, Task is already in the list'
+  } else if (op === 'r' && !isTaskInList) { // Checks Remove Buton
+    return 'Oops, Task is NOT in the list'
   }
 
-  return true;
+  return true
 }
 
-function callAjax() {
+function callAjax () {
   $.get('http://localhost:3000/tasks')
     .done(data => {
-      window.alert(data);
-    });
+      window.alert(data)
+    })
 }
